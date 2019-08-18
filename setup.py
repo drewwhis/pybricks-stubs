3eae7a5ec5040305378661d09d1f0672e3b9e163
Lawrence (Drew) Whisenant <drewwhis@users.noreply.github.com>
2019-08-18 01:02:18 -0500

    update package name

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-        name="pybricks-fll",
-        version="0.3",
+        name="pybricks-stubs",
+        version="0.1",
         author="Lawrence (Drew) Whisenant",
         author_email="dwhisenant@FIRSTinAlabama.org",
-        description="An interfaces package to write EV3 Python code.",
+        description="An stubs package to write EV3 Python code.",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        url="https://github.com/drewwhis/pybricks-fll",
+        url="https://github.com/drewwhis/pybricks-stubs",
         packages=setuptools.find_packages(),
         classifiers=[
             "Programming Language :: Python :: 3",
